e725abed486270745d799ce380ec2faafaa3f4ef
JimmyWallener <jimmy.wallener@protonmail.com>
2024-05-11 10:52:33 +0200

    small fixes

--- FE21-js2-slutprojekt-hotpink/src/script/modules/db.ts
+++ FE21-js2-slutprojekt-hotpink/src/script/modules/db.ts
@@ -2,18 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD33ogB_ziq_KJthXw6Eymfe85XyM28Qa0',
-  authDomain: 'hotpink-a9449.firebaseapp.com',
-  databaseURL:
-    'https://hotpink-a9449-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'hotpink-a9449',
-  storageBucket: 'hotpink-a9449.appspot.com',
-  messagingSenderId: '539798814024',
-  appId: '1:539798814024:web:cd59108fc95c3722efc77d',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app);
-
-
